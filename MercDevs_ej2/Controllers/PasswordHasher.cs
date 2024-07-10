8fa95632e68b4b3047bc8eeae24de94f8cd3f533
moontivac10n <lucas.venegas.jeldrez@cftmail.cl>
2024-07-10 00:21:02 -0400

    Avance fase 3,4,5

---
+++ MercDevs_ej2/Controllers/PasswordHasher.cs
@@ -0,0 +1,13 @@
+﻿using BCrypt.Net;
+
+public class PasswordHasher
+{
+    public static void Main()
+    {
+        string password = "futbol"; // Aquí colocas la contraseña que quieres hashear
+        string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
+
+        Console.WriteLine("Contraseña original: " + password);
+        Console.WriteLine("Contraseña hasheada: " + hashedPassword);
+    }
+}
